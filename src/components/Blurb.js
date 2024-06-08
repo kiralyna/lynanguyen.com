5d1cbd32316be5606155132a27e57c2087bcbe4f
kiralyna <LynaNguyen4131@gmail.com>
2024-06-08 15:41:05 -0700

    finally fixed contact box sizing and formatting

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import data from '../data.json';
+import '../App.css';
 
 function Blurb() {
+    const { email, linkedin, instagram, addFriendIcon, favoritesIcon, messageIcon } = data;
+
     return (
-        <div>Blurb Section</div>
+        <div id="tiles">
+            <div id="blurb">
+                <table id="contact-me">
+                    <tbody id="contact-body">
+                        <tr id="title">
+                            <th id="contact-title">
+                                <h3>Contacting Lyna</h3>
+                            </th>
+                        </tr>
+                        <tr id="email">
+                            <td id="contact-email">
+                                <img alt ="mail icon" src = {messageIcon}></img>
+                                <a href = {email} >Send Message</a><br/>
+                            </td>
+                        </tr>
+                        <tr id="linkedin">
+                            <td id="contact-linkedin">
+                                <img alt ="add icon" src = {addFriendIcon}></img>
+                                <a href = {linkedin} target = "_blank" >Add to Friends</a><br/>
+                            </td>
+                        </tr>
+                        <tr id="insta">
+                            <td id="contact-insta">
+                                <img alt ="add icon" src = {favoritesIcon}></img>
+                                <a href = {instagram} target = "_blank" >Add to Favorites</a><br/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
-export default Blurb 
+export default Blurb
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,23 +6,35 @@ function Contact() {
     const { email, linkedin, instagram, addFriendIcon, favoritesIcon, messageIcon } = data;
 
     return (
-        <div id="tiles">
-            <div id="contacting-lyna">
-                <h3>Contacting Lyna</h3>
-            </div>
-            <div id="contact-body">
-                <div id="contact-email">
-                    <img alt ="mail icon" src = {messageIcon}></img>
-                    <a href = {email} >Send Message</a><br/>
-                </div>
-                <div id="contact-linkedin">
-                    <img alt ="add icon" src = {addFriendIcon}></img>
-                    <a href = {linkedin} target = "_blank" >Add to Friends</a><br/>
-                </div>
-                <div id="contact-insta">
-                    <img alt ="add icon" src = {favoritesIcon}></img>
-                    <a href = {instagram} target = "_blank" >Add to Favorites</a><br/>
-                </div>
+        <div id="contact-tile">
+            <div id="contact">
+                <table id="contact-me">
+                    <tbody id="contact-body">
+                        <tr id="title">
+                            <th id="contact-title">
+                                <h3>Contacting Lyna</h3>
+                            </th>
+                        </tr>
+                        <tr id="email">
+                            <td id="contact-email">
+                                <img alt ="mail icon" src = {messageIcon}></img>
+                                <a href = {email} >Send Message</a><br/>
+                            </td>
+                        </tr>
+                        <tr id="linkedin">
+                            <td id="contact-linkedin">
+                                <img alt ="add icon" src = {addFriendIcon}></img>
+                                <a href = {linkedin} target = "_blank" >Add to Friends</a><br/>
+                            </td>
+                        </tr>
+                        <tr id="insta">
+                            <td id="contact-insta">
+                                <img alt ="add icon" src = {favoritesIcon}></img>
+                                <a href = {instagram} target = "_blank" >Add to Favorites</a><br/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
