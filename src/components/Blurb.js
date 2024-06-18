7b46c9935c29326d71dde6147660afe9e9a5544c
kiralyna <LynaNguyen4131@gmail.com>
2024-06-17 18:17:45 -0700

    standardizing layout body tiles style for uniform look, started on blurb tile

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -3,35 +3,17 @@ import data from '../data.json';
 import '../App.css';
 
 function Blurb() {
-    const { email, linkedin, instagram, addFriendIcon, favoritesIcon, messageIcon } = data;
+    const {} = data;
 
     return (
         <div id="tiles">
             <div id="blurb">
-                <table id="contact-me">
-                    <tbody id="contact-body">
+                <table id="tile-table">
+                    <tbody id="tile-body">
                         <tr id="title">
-                            <th id="contact-title">
-                                <h3>Contacting Lyna</h3>
+                            <th id="tile-title">
+                                <h3>About Me</h3>
                             </th>
-                        </tr>
-                        <tr id="email">
-                            <td id="contact-email">
-                                <img alt ="mail icon" src = {messageIcon}></img>
-                                <a href = {email} >Send Message</a><br/>
-                            </td>
-                        </tr>
-                        <tr id="linkedin">
-                            <td id="contact-linkedin">
-                                <img alt ="add icon" src = {addFriendIcon}></img>
-                                <a href = {linkedin} target = "_blank" >Add to Friends</a><br/>
-                            </td>
-                        </tr>
-                        <tr id="insta">
-                            <td id="contact-insta">
-                                <img alt ="add icon" src = {favoritesIcon}></img>
-                                <a href = {instagram} target = "_blank" >Add to Favorites</a><br/>
-                            </td>
                         </tr>
                     </tbody>
                 </table>
